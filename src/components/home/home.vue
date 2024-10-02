79adcdd9458bde11fa3cfe8b32f09c2778e3ed69
trevor <thibblen@kpaonline.com>
2024-10-02 09:00:30 -0700

    cleanup with commented examples

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -15,23 +15,11 @@
 
 <script>
 // import Hero from "./components/hero/hero.vue";
-// import Stats from "./components/stats/stats.vue";
-// import Reporting from "./components/reporting/reporting.vue";
-// import Training from "./components/training/training.vue";
-// import Modules from "./components/modules/modules.vue";
-// import Videos from "src/components/videos/videos.vue";
-// import LogosRow from 'src/components/customer-logos/row/row.vue';
 
 export default {
   props: ["dealTimeFrame","deal"],
   components: {
     // hero: Hero,
-    // stats: Stats,
-    // reporting: Reporting,
-    // training: Training,
-    // modules: Modules,
-    // LogosRow,
-    // Videos,
   },
 };
 </script>
