fe55131aad3808037b72cadf5e6f1f90e1c4898d
Jon Schwamman <jschwamman@kpa.io>
2024-10-03 10:47:37 -0600

    Updating nav bar

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -4,8 +4,8 @@
 <template>
   <div class="page-nav-container">
     <div class="page-nav" :class="{fixed:fixed,'fixed-in':fixedIn,'fixed-out':fixedOut}">
-      <div class="body">
-        <h1 class="text-4xl ml-4">🌐 Smart Living</h1>
+      <div class="body mt-3">
+        <h1 class="text-3xl ml-4 font-extrabold" style="color: #5C5C5C">♻️ Smart Living</h1>
         <div style="flex:1;"></div>
         <ul class="nav-links mr-4">
           <li>
@@ -16,15 +16,32 @@
               </a>
             </div> -->
             <a class="cart" href="/account">
-              <span class="text-white text-md rounded-md bg-green-500 p-2
-                shadow-sm text-3xl hover:shadow-lg
+              <span style="background-color: #5C5C5C" class="text-white text-md rounded-md py-2 px-4
+                shadow-sm text-xl hover:shadow-lg
                 -mt-4
-              ">236 Pts</span>
+              ">Logout</span>
             </a>
           </li>
+        </ul>
+       
+      </div>
+      <div class="nav-links-pill mt-4">
+        <ul class="flex flex-row w-4/5 m-auto pt-3.5">
+          <li class="flex-auto"><a href="/">Home</a></li>
+          <li class="flex-auto">・</li>
+          <li class="flex-auto"><a href="/">Purchase History</a></li>
+          <li class="flex-auto">・</li>
+          <li class="flex-auto"><a href="/">Rewards</a></li>
+          <li class="flex-auto">・</li>
+          <li class="flex-auto"><a href="/">Account</a></li>
+          <li class="flex-auto">・</li>
+          <li class="flex-auto"><a href="/">🪙 215</a></li>
         </ul>
       </div>
     </div>
     <div class="page-nav-placeholder" v-if="fixed"></div>
   </div>
 </template>
+
+/* Group 8 */
+
